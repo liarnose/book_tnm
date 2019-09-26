72b0821de6fa9df06505c9400c68551be120f91b
liarnose <liarnose@gmail.com>
2019-09-26 14:38:26 +0900

    metalsmith-postcss導入

---
+++ script/build-theme.js
@@ -0,0 +1,24 @@
+var metalsmith = require('metalsmith');
+var postcss = require('metalsmith-postcss');
+var watch = require('metalsmith-watch');
+
+metalsmith(__dirname)
+  .source('../theme-src')
+  .destination('../theme-dest')
+  .clean(true)
+  .use(postcss({
+    plugins: {
+      'postcss-nested': {},
+      'postcss-custom-properties': {
+         preserve: false,
+      },
+    }
+  }))
+  .use(watch({
+      paths: {
+        "${source}/**/*": true,
+      },
+  }))
+  .build(function(err) {
+    if (err) throw err;
+  });
